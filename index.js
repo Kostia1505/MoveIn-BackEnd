dfdb948a1359b464a03263ab21f31d4ba284579d
Kostia1505 <kostiantyn.holovenko.ri.2024@lpnu.ua>
2025-04-02 20:59:35 +0300

    index.js update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require('http')
+const { Sequelize } = require('sequelize');
+
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'})
@@ -11,3 +13,12 @@ const HOST = 'localhost'
 server.listen(PORT, HOST, () =>{
     console.log(`Сервер: http://${HOST}:${PORT}`)
 })
+
+const sequelize = new Sequelize('movein', 'kostia', 'ZhekaFat69', {
+    host: 'localhost',
+    dialect: 'postgres',
+});
+
+sequelize.authenticate()
+    .then(() => console.log('Connected to PostgreSQL'))
+    .catch(err => console.error('Connection error:', err));
